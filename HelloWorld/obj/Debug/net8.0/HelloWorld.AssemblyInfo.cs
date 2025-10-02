2b14de581ebdd47fc994404263236e95a150a4d0
Philip Oxelius <philip.oxelius@gmail.com>
2025-10-02 14:14:18 +0200

    Hejsan

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e52d7c0b5a779603b92aa35508adfff409188d08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beeaaceb126ac28cf06db2b7011f0453ac4bb403")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
